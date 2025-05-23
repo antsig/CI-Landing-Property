8434ffc89e992e22b117a3dbe448182e804169e9
Ant Sigid Priharsanto <93061220+antsig@users.noreply.github.com>
2025-05-24 06:28:39 +0800

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'compro',
+        'database' => 'db_ci4',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
